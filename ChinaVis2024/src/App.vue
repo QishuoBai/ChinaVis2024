1e83d2b9c7f13ac98bc544705ed7d166fcb3bd04
QishuoBai <qishuo-bai@sjtu.edu.cn>
2024-06-17 16:22:16 +0800

    改了一下布局的代码风格

--- ChinaVis2024/src/App.vue
+++ ChinaVis2024/src/App.vue
@@ -1,44 +1,26 @@
 <template>
-  <div>
-    <div id="header">
-      <h3>时序多变量教育数据可视分析系统</h3>
+  <div class="d-flex flex-column h-screen w-screen">
+    <div id="header" class="border-b-thin text-h6 pa-1 pl-3">
+      时序多变量教育数据可视分析系统
     </div>
-    <div style="width: 100vw;height: 45vh;">
-      <div id="ClusterView"></div>
-      <div id="SubmitView">
-        <Submit />
+    <div class="flex-grow-1 d-flex flex-column w-100">
+      <div class="h-50 d-flex flex-row">
+        <div id="ClusterView" style="width:30%" class="border-thin"></div>
+        <div id="SubmitView" class="flex-grow-1 border-sm">
+          <Submit />
+        </div>
       </div>
-    </div>
-    <div style="width: 100vw;height: 45vh;">
-      <div id="PortraitView"></div>
-      <div id="TitleView"></div>
+      <div class="h-50 d-flex flex-row">
+        <div id="PortraitView" style="width:30%" class="border-sm"></div>
+        <div id="TitleView" class="flex-grow-1 border-sm"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  //
+//
 </script>
-<style scoped>  
-#header {
-  /* background-color: "red"; */
-  width: 100vw;
-  height: 9vh;
-  border: 1px solid #ddd;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-#ClusterView, #PortraitView{
-  width: 30%;
-  height: 100%;
-  border: 1px solid #ddd;
-  float: left;
-}
-#SubmitView, #TitleView {
-  width: 70%;
-  height: 100%;
-  border: 1px solid #ddd;
-  float: left;
-}
+<style scoped>
+
 </style>
